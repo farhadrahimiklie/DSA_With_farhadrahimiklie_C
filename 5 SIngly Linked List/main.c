2cff49f3c367d7d348e91ac3d59d3d7462c28757
farhadrahimiklie <farhadrahimiklie786@gmail.com>
2025-11-01 14:16:16 +0430

    Singly linked list completely done.

--- 5 SIngly Linked List/main.c
+++ 5 SIngly Linked List/main.c
@@ -1,120 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// This is the Singly Linked List
-
+// We Explore Singly Linked List
 typedef struct Node
 {
     int data;
-    struct Node* next;
+    struct Node* next; // pointer
 } Node;
 
-Node* head = NULL; // Initialze the Head Node with Null value
+Node* head = NULL;
 
-Node* CreateNode(int data){
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
-}
+Node* CreateNode(int data);
+void InsertAtBegining(Node** head, int data);
+void InsertAtEnd(Node** head, int data);
+void InsertAtIndex(Node** head, int data, int index);
+void DeleteAtBegining(Node** head);
+void DeleteAtEnd(Node** head);
+void DeleteAtIndex(Node** head, int index);
+int Search(Node** head, int key);
+void Display(Node* head);
 
-Node* InsertAtBegining(Node* head, int data){
-    Node* newNode = CreateNode(data);
-    newNode->data = data;
-    newNode->next = head;
-    head = newNode;
-    return head;
-}
+int main() {
+    InsertAtBegining(&head, 10);
+    InsertAtBegining(&head, 20);
+    InsertAtBegining(&head, 30);
+    InsertAtBegining(&head, 40);
 
-Node* InsertAtEnd(Node* head, int data){
-    Node* newNode = CreateNode(data);
-    newNode->data = data;
-    Node* temp = head;
+    // InsertAtEnd(&head, 100);
 
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    temp->next = newNode;
-    newNode->next = NULL;
-    return head;
-}
+    // InsertAtIndex(&head, 400, 1);
 
-Node* InsertAtIndex(Node* head, int data, int index){
-    Node* newNode = CreateNode(data);
-    newNode->data = data;
+    // DeleteAtBegining(&head);
+
+    // DeleteAtEnd(&head);
+
+    // DeleteAtIndex(&head, 1);
+
+
+    Display(head);
+
+    // int result = Search(&head, 40);
+    // if (result != -1)
+    // {
+    //     printf("Found at Index %d \n", result);
+    // }else{
+    //     printf("Not Founded\n");
+    // }
     
-    Node* temp = head;
-    Node* p = head->next;
-
-
-    for (int i = 0; i < index-1; i++)
-    {
-        temp = temp->next;
-        p  = p->next;
-    }
-    temp->next = newNode;
-    newNode->next = p;
-    return head;
-    
+    return 0;
 }
 
-Node* DeleteAtBegining(Node* head){
-    if (head->next == NULL)
-    {
-        printf("Linked List is Empty Nothing to delete.");
-    }
-    Node* temp = head;
-    head = head->next;
-    free(temp);
-    return head;
-}
-
-Node* DeleteAtEnd(Node* head){
-    Node* temp = head->next;
-    Node* prev = head;
-
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-        prev = prev->next;
-    }
-    free(temp);
-    prev->next = NULL;
-    return head;
-}
-
-Node* DeleteAtIndex(Node* head, int index){
-    Node* p = head;
-    Node* q = head->next;
-
-    for (int i = 0; i < index-1; i++)
-    {
-        p = p->next;
-        q = q->next;
-    }
-    p->next = q->next;
-    free(q);
-    return head;
-}
-
-int Search(int key){
-    Node* temp = head;
-    int index = 0;
-
-    while (temp != NULL)
-    {
-        if (temp->data == key)
-        {
-            return index;
-        }
-        temp = temp->next;
-        index++;
-    }
-    return -1;
-}
-
-void display(Node* head){
+void Display(Node* head){
     Node* temp = head;
 
     while (temp != NULL)
@@ -125,34 +60,139 @@ void display(Node* head){
     printf("NULL \n");
 }
 
-int main(){
+Node* CreateNode(int data){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL)
+    {
+        printf("Allocation Failed! ");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
 
-    head = InsertAtBegining(head, 10);
-    head = InsertAtBegining(head, 20);
-    head = InsertAtBegining(head, 30);
-    head = InsertAtBegining(head, 40);
-
-    // head = InsertAtEnd(head, 50);
-    // head = InsertAtEnd(head, 60);
-
-    // head = InsertAtIndex(head, 100, 2);
-
-    // head = DeleteAtBegining(head);
-
-    // head = DeleteAtEnd(head);
-
-    // head = DeleteAtIndex(head, 2);
-
-    // display(head);
-
-    // int search = Search(34);
-    // if (search != -1)
-    // {
-    //     printf("I found the data %d \n", search);
-    // }else{
-    //     printf("We could'nt found any data in this areas %d \n", search);
-    // }
+void InsertAtBegining(Node** head, int data){
+    Node* newNode = CreateNode(data);
+    if (*head == NULL)
+    {
+        *head = newNode;
+        return;
+    }
     
+    newNode->data = data;
+    newNode->next = *head;
+    *head = newNode;
+    return;
+}
 
-    return 0;
+void InsertAtEnd(Node** head, int data){
+    Node* newNode = CreateNode(data);
+    if (*head == NULL)
+    {
+        *head = newNode;
+        return;
+    }
+    Node* temp = *head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    newNode->data = data;
+    temp->next = newNode;
+    return;
+}
+
+void InsertAtIndex(Node** head, int data, int index){
+    Node* newNode = CreateNode(data);
+    if (*head == NULL)
+    {
+        *head = newNode;
+        return;
+    }
+    if (index == 0)
+    {
+        InsertAtBegining(head, data);
+        return;
+    }
+    Node* temp = *head;
+
+    for (int i = 0; i < index-1; i++)
+    {
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+    return;
+}
+
+void DeleteAtBegining(Node** head){
+    if ((*head)->next == NULL)
+    {
+        free(head);
+        *head = NULL;
+    }
+    Node* temp = *head;
+    *head = (*head)->next;
+    free(temp);
+    return;
+}
+
+void DeleteAtEnd(Node** head){
+    if ((*head)->next == NULL)
+    {
+        free(head);
+        *head = NULL;
+    }
+    Node* temp = (*head)->next;
+    Node* prev = *head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+        prev = prev->next;
+    }
+    prev->next = NULL;
+    free(temp);
+    return;
+}
+
+void DeleteAtIndex(Node** head, int index){
+    if ((*head)->next == NULL)
+    {
+        free(head);
+        *head = NULL;
+    }
+    if (index == 0)
+    {
+        DeleteAtBegining(head);
+        return;
+    }
+    
+    Node* temp = *head;
+    Node* q = (*head)->next;
+    for (int i = 0; i < index-1; i++)
+    {
+        temp = temp->next;
+        q = q->next;
+    }
+    temp->next = q->next;
+    q->next = NULL;
+    free(q);
+    return;
+}
+
+int Search(Node** head, int key){
+    Node* temp = *head;
+    int i = 0;
+
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            return i;
+        }
+        temp = temp->next;
+        i++;
+    }
+    return -1;
 }
--- practice.c
+++ practice.c
@@ -1,7 +1,198 @@
 #include <stdio.h>
 #include <stdlib.h>
+// We Explore Singly Linked List
+typedef struct Node
+{
+    int data;
+    struct Node* next; // pointer
+} Node;
+
+Node* head = NULL;
+
+Node* CreateNode(int data);
+void InsertAtBegining(Node** head, int data);
+void InsertAtEnd(Node** head, int data);
+void InsertAtIndex(Node** head, int data, int index);
+void DeleteAtBegining(Node** head);
+void DeleteAtEnd(Node** head);
+void DeleteAtIndex(Node** head, int index);
+int Search(Node** head, int key);
+void Display(Node* head);
 
 int main() {
+    InsertAtBegining(&head, 10);
+    InsertAtBegining(&head, 20);
+    InsertAtBegining(&head, 30);
+    InsertAtBegining(&head, 40);
+
+    // InsertAtEnd(&head, 100);
+
+    // InsertAtIndex(&head, 400, 1);
+
+    // DeleteAtBegining(&head);
+
+    // DeleteAtEnd(&head);
+
+    // DeleteAtIndex(&head, 1);
+
+
+    Display(head);
+
+    // int result = Search(&head, 40);
+    // if (result != -1)
+    // {
+    //     printf("Found at Index %d \n", result);
+    // }else{
+    //     printf("Not Founded\n");
+    // }
     
     return 0;
+}
+
+void Display(Node* head){
+    Node* temp = head;
+
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL \n");
+}
+
+Node* CreateNode(int data){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL)
+    {
+        printf("Allocation Failed! ");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void InsertAtBegining(Node** head, int data){
+    Node* newNode = CreateNode(data);
+    if (*head == NULL)
+    {
+        *head = newNode;
+        return;
+    }
+    
+    newNode->data = data;
+    newNode->next = *head;
+    *head = newNode;
+    return;
+}
+
+void InsertAtEnd(Node** head, int data){
+    Node* newNode = CreateNode(data);
+    if (*head == NULL)
+    {
+        *head = newNode;
+        return;
+    }
+    Node* temp = *head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    newNode->data = data;
+    temp->next = newNode;
+    return;
+}
+
+void InsertAtIndex(Node** head, int data, int index){
+    Node* newNode = CreateNode(data);
+    if (*head == NULL)
+    {
+        *head = newNode;
+        return;
+    }
+    if (index == 0)
+    {
+        InsertAtBegining(head, data);
+        return;
+    }
+    Node* temp = *head;
+
+    for (int i = 0; i < index-1; i++)
+    {
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+    return;
+}
+
+void DeleteAtBegining(Node** head){
+    if ((*head)->next == NULL)
+    {
+        free(head);
+        *head = NULL;
+    }
+    Node* temp = *head;
+    *head = (*head)->next;
+    free(temp);
+    return;
+}
+
+void DeleteAtEnd(Node** head){
+    if ((*head)->next == NULL)
+    {
+        free(head);
+        *head = NULL;
+    }
+    Node* temp = (*head)->next;
+    Node* prev = *head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+        prev = prev->next;
+    }
+    prev->next = NULL;
+    free(temp);
+    return;
+}
+
+void DeleteAtIndex(Node** head, int index){
+    if ((*head)->next == NULL)
+    {
+        free(head);
+        *head = NULL;
+    }
+    if (index == 0)
+    {
+        DeleteAtBegining(head);
+        return;
+    }
+    
+    Node* temp = *head;
+    Node* q = (*head)->next;
+    for (int i = 0; i < index-1; i++)
+    {
+        temp = temp->next;
+        q = q->next;
+    }
+    temp->next = q->next;
+    q->next = NULL;
+    free(q);
+    return;
+}
+
+int Search(Node** head, int key){
+    Node* temp = *head;
+    int i = 0;
+
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            return i;
+        }
+        temp = temp->next;
+        i++;
+    }
+    return -1;
 }
